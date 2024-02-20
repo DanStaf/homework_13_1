038cbf6e60ce69d43a79f2c88812438922a15546
DanStaf <danstaf@mail.ru>
2024-02-20 18:54:45 +0300

    additional task added: function loading classes from json

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import my_classes
+import json
+
+
+def get_classes_from_file():
+
+    with open("../products.json", 'r', encoding='utf-8') as f:
+        reading_result = json.loads(f.read())
+
+    categories = []
+
+    for each_result in reading_result:
+        if each_result:
+            goods = []
+            for each_product in each_result['products']:
+                if each_product:
+                    new_product = my_classes.Product(each_product['name'],
+                                                     each_product['description'],
+                                                     each_product['price'],
+                                                     each_product['quantity'])
+                    goods.append(new_product)
+
+            new_category = my_classes.Category(each_result['name'],
+                                               each_result['description'],
+                                               goods)
+
+            categories.append(new_category)
+
+    return categories
+
+
+check = get_classes_from_file()
+[print(item) for item in check]
+
+print("number_of_categories:", my_classes.Category.number_of_categories)
+print("number_of_products_types:", my_classes.Category.number_of_products_types)
+print("unique_product_names:", my_classes.Category.unique_product_names)
